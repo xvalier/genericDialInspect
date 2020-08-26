2035b855f436b46311b682bb2958cc201a459c5f
xvalier <nikilsunilkumar@gmail.com>
2020-08-26 00:33:40 -0400

    Added colors, default dims, mouse warp ability. Refactored with numpy

--- rectangle.py
+++ rectangle.py
@@ -1,24 +1,27 @@
 import cv2 as cv 
 import numpy as np 
 import os
+import time
 
 #Example Script Code------
 def main():
     path = os.getcwd() + '\\input\\test.png'
     img = cv.imread(path, 1)
-    view = BoundingBox(img, 'Slot', [(0,255,0), (255,255,0)])
+    view = BoundingBox(img, 'Slot', 500, 60, (0,255,0))
     start, end = view.modify()
-    print('{0},{1}'.format(start, end))
       
 class BoundingBox:
     image = None 
     name = ""
-    colors = [(0, 255, 0), (255,255,0)] #Colors for bounding box and markers
-    thickness = 20
-    start  = [100,100]
-    end    = [300, 300]
-    anchor = [0,0]
-    markers = [
+    color= (0,255,0)
+    thickness = 5 
+    default_width = 0
+    default_height= 0
+    start  = np.array([100,100])
+    end    = np.array([300, 300])
+    anchor = np.array([0,0])
+    marker_colors = [(255,0,255) for i in range(0,8)]
+    markers = np.array([
         [[0,0],[0,0]],  #Topleft
         [[0,0],[0,0]],  #Topmid
         [[0,0],[0,0]],  #Topright
@@ -26,18 +29,21 @@ class BoundingBox:
         [[0,0],[0,0]],  #Midright
         [[0,0],[0,0]],  #Bottomleft
         [[0,0],[0,0]],  #Bottommid
-        [[0,0],[0,0]],  #Bottomright
-    ]
-    marker_flags = [False, False, False, False, False, False, False, False]
+        [[0,0],[0,0]]   #Bottomright
+    ])
+    marker_flags = np.full((8), False, dtype=bool) 
     drag_flag = False
     
     #Constructor sets image/color, and marker positions
-    def __init__(self, image, name, colors):
+    def __init__(self, image, name, width, height, color):
         self.image = image 
         self.name = name
-        self.colors = colors
-        for point in range(0,len(self.markers)):
-            self.refresh_marker(point)
+        self.color = color
+        self.default_width  = width
+        self.default_height  = height
+        self.end = np.array([100+self.default_width, 100+self.default_height])
+        #Store in a temporary variable to avoid printing to terminal
+        i = [self.refresh_marker(point) for point in np.arange(self.markers.shape[0])]
             
     #Method to launch window to modify bounding box positions
     def modify(self):
@@ -46,7 +52,6 @@ class BoundingBox:
         cv.setMouseCallback(self.name, self.on_mouse, self)
         self.draw_rectangle()
         cv.waitKey()
-        return [self.start, self.end]
 
     #Event handler for mouse operations
     def on_mouse(self, event, x,y, flags, view):
@@ -57,65 +62,98 @@ class BoundingBox:
             self.mouse_up()
         if event == cv.EVENT_MOUSEMOVE:
             self.mouse_move(x,y)
+        if event == cv.EVENT_RBUTTONUP:
+            w = int((self.end[0]-self.start[0])/2)
+            h = int((self.end[1]-self.start[1])/2)
+            self.mouse_warp(x,y, w, h)
+        if event == cv.EVENT_RBUTTONDBLCLK:
+            w = int(self.default_width/2)
+            h = int(self.default_height/2)
+            self.mouse_warp(x,y, w, h)
         if event == cv.EVENT_LBUTTONDBLCLK:
             self.mouse_doubleclick()
     
-    #Operations for when left mouse button is pressed down
+    #Event when left mouse button is pressed
     def mouse_down(self, x,y):
-        coordinates = [x,y]
+        coordinates = np.array([x,y])
         #If user pressed a specific corner, set that corner's flag to True (allows changing of box size)
-        for point in range(0, len(self.markers)):
+        for point in np.arange(self.markers.shape[0]):
             self.marker_flags[point] = self.inside_box(coordinates, self.markers[point])
+            self.marker_colors[point] = (125,255,255) if self.marker_flags[point] else (255,0,255)
         #If user pressed inside the bounding box, set hold flag to True (moves box without changing size)
-        self.drag_flag  = True if self.inside_box(coordinates, [self.start, self.end]) else False 
+        if not True in self.marker_flags:
+            self.drag_flag = self.inside_box(coordinates, np.array([self.start, self.end]))
         if True in self.marker_flags or self.drag_flag:
             self.anchor = coordinates
 
-    #Operation for when left mouse button is released
+    #Event when left mouse button is released
     def mouse_up(self):
         #Reset all flags, switch start/end points if they were reversed during size adjustment
         self.drag_flag = False 
-        self.marker_flags = [False for flag in self.marker_flags]
-        for i in range(0,2):
-            if (self.end[i] - self.start[i]) < 0:
-                temp = self.start[i]
-                self.start = (self.end[i], self.start[i])
-                self.end = (temp, self.end[i])
+        self.marker_flags = np.full((8), False, dtype=bool) 
+        self.marker_colors = [(255,0,255) for i in range(0,8)]
+        x0,y0 = self.start 
+        x1,y1 = self.end
+        if (x1-x0) < 0:
+            self.start[0] = x1
+            self.end[0]   = x0
+        if (y1-y0) < 0:
+            self.start[0] = y1
+            self.end[0]   = y0
+        self.draw_rectangle()
+    
+    #Event when right mouse button is pressed. Recreates box to cursor point
+    def mouse_warp(self, x,y, w, h):
+        #Clip xy cursor point so that box doesn't go outside FOV
+        within_image    = np.array([
+            True if x-w > 0 else False,
+            True if x+w < len(self.image[0]) else False,
+            True if y-h > 0 else False,
+            True if y+h < len(self.image) else False
+        ])
+        if within_image.all():
+            self.start  = np.array([x-w,y-h])
+            self.end    = np.array([x+w,y+h])
+        i = [self.refresh_marker(point) for point in np.arange(self.markers.shape[0])]
+        self.draw_rectangle()
 
-    #End modification window if mouse is double clicked
+    #Event when left mouse button is double clicked. Used to end modification
     def mouse_doubleclick(self):
         cv.destroyWindow(self.name)
-        
-    #Operation for when mouse is moved
+
+    #Event when mouse is moved around
+    #TODO: This is the function to make more efficient
+    #Can probably make this smoother by not basing it on an offset (use actual xy cursor loc to prevent delays)
     def mouse_move(self, x,y):
-        if self.drag_flag or (True in self.marker_flags):
-            offset = [x - self.anchor[0], y - self.anchor[1]]
+        if True in self.marker_flags or self.drag_flag:
+            offset = np.array([x - self.anchor[0], y - self.anchor[1]])
             #If mouse moved while button pressed inside bounding box, keep entire offset
             if self.drag_flag:
-                offset_start = offset
-                offset_end   = offset
+                offset_start = offset_end = offset
             #If mouse moved while button pressed on a marker, adjust bounding box centroid
             #Adjust the centroid by masking out parts of offset based on which marker was used to resize
-            for point in range(0,len(self.markers)):
+            for point in np.arange(self.markers.shape[0]):
                 if self.marker_flags[point]:
                     point = point + 1 if point > 3 else point
-                    offset_start = (offset[0] if not (point%3) else 0, offset[1] if (point < 3) else 0)
-                    offset_end   = (offset[0] if not ((point+1)%3) else 0, offset[1] if (point > 5) else 0)
+                    offset_start = np.array([offset[0] if not (point%3) else 0, offset[1] if (point < 3) else 0])
+                    offset_end   = np.array([offset[0] if not ((point+1)%3) else 0, offset[1] if (point > 5) else 0])
             #Adjust bounding box and marker positions based on offsets, make sure bounding box is within image border
             new_start = np.add(self.start, offset_start)
-            new_end  = np.add(self.end, offset_end)
-            within_image    = [False, False, False, False]
-            within_image[0] = True if new_start[0] > 0 else False 
-            within_image[1] = True if new_end[0] < len(self.image[0]) else False 
-            within_image[2] = True if new_start[1] > 0 else False 
-            within_image[3] =  True if new_end[1] < len(self.image) else False  
-            if not False in within_image:
+            new_end   = np.add(self.end, offset_end)
+            #TODO: Check if doing inside_box function x2 is faster than this
+            within_image    = np.array([
+                True if new_start[0] > 0 else False,
+                True if new_end[0] < len(self.image[0]) else False,
+                True if new_start[1] > 0 else False,
+                True if new_end[1] < len(self.image) else False
+            ])
+            if within_image.all():
                 self.start = new_start
                 self.end = new_end
-            for point in range(0,len(self.markers)):
-                self.refresh_marker(point)
+            #Refresh markers based on new centroid. Store in a temporary variable to avoid printing to terminal
+            i = [self.refresh_marker(point) for point in np.arange(self.markers.shape[0])]
             self.draw_rectangle()
-        
+
     #Update marker position based on bounding box coordinates  
     def refresh_marker(self, point):
         x0, y0 = self.start
@@ -129,25 +167,26 @@ class BoundingBox:
         center_x = x1 if not ((point+1)%3) else center_x
         #Create start and end points for marker rectangle
         point = point - 1 if point > 3 else point
-        self.markers[point][0] = [center_x-self.thickness, center_y-self.thickness]
-        self.markers[point][1] = [center_x+self.thickness, center_y+self.thickness]
+        self.markers[point] = np.array([ 
+            [center_x-self.thickness, center_y-self.thickness],
+            [center_x+self.thickness, center_y+self.thickness]
+        ])
   
-    #Check if given coordinates are start/end points of bounding box
+    #Check if given coordinates are start/end points of provided box
     def inside_box(self, coordinates, box):
         x0, y0 = box[0]
         x1, y1 = box[1]
-        inside_x = (coordinates[0] in range(x0, x1))
-        inside_y = (coordinates[1] in range(y0, y1))
-        result   = inside_x and inside_y
+        result = (x0 < coordinates[0] < x1) & (y0< coordinates[1] < y1)
         return result
 
     #Draw rectangle with markers on image
     def draw_rectangle(self):
         #Perhaps try drawing on an image of equal size to raw image for graphics, then adding the two?
         image = self.image.copy()
-        image = cv.rectangle(image, tuple(self.start), tuple(self.end), self.colors[0], self.thickness-8)
-        for marker in self.markers:
-            image = cv.rectangle(image, tuple(marker[0]), tuple(marker[1]), self.colors[1], self.thickness)
+        cv.rectangle(image, tuple(self.start), tuple(self.end), self.color, self.thickness)
+        for point in range(0,8):
+            cv.rectangle(image, tuple(self.markers[point][0]), tuple(self.markers[point][1]), self.marker_colors[point], self.thickness+5)
         cv.imshow(self.name, image)
+        cv.waitKey(30)
     
 main()
